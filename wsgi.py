17259fddeb5a684264d88066abb9e3aaf0b647fd
Chen Yian <chenyian@example.com>
2026-04-27 10:09:38 +0800

    v1.0.25 優化部署啟動速度

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,17 @@
 import sys
 import os
 
-project_home = os.path.dirname(os.path.abspath(__file__))
+project_home = '/home/a14008/cad-web-app'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
-
 os.chdir(project_home)
 
-from a2wsgi import ASGIMiddleware
-from main import app as fastapi_app
+_application = None
 
-application = ASGIMiddleware(fastapi_app)
+def application(environ, start_response):
+    global _application
+    if _application is None:
+        from a2wsgi import ASGIMiddleware
+        from main import app as fastapi_app
+        _application = ASGIMiddleware(fastapi_app)
+    return _application(environ, start_response)
